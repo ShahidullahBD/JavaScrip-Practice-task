f1c79fb18842d68e4e7febfabf6440aafd076add
ShahidullahBD <shahidullahkorea@gmail.com>
2026-01-28 19:58:52 +0900

    first commit of object.js

---
+++ object.js
@@ -0,0 +1,12 @@
+const computer = {
+    brand: 'MS',
+    origin: 'USA',
+    price: 30000,
+    monitor: 'hp'
+}
+// console.log(computer);
+
+const keys = Object.keys(computer);
+console.log(keys);
+const values = Object.values(computer);
+console.log(values);
